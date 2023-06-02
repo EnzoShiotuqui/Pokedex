4c3f42af94fe8cee5b7228b4e43d50861e358233
EnzoShiotuqui <EnzoMshiotuqui@gmail.com>
2023-06-02 11:42:01 -0300

    js change

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
   if (APIResponse.status === 200) {
-    const data = await APIResponse.json();
+    const data = await APIResponse.json(); 
     return data;
   }
 }
